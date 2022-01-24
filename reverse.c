dfed153496b0068d487ecc17adc7f5ee3dbd406f
mteiska <mika123450@hotmail.com>
2022-01-24 23:58:28 +0200

    Toimiva linkattu lista

--- reverse.c
+++ reverse.c
@@ -1,40 +1,65 @@
-    #include <stdlib.h> 
-    #include<stdio.h> // define the header file 
-    #include<string.h>
-    
+#include <stdlib.h>
+#include <stdio.h> // define the header file
+#include <string.h>
 
+typedef struct lista
+{
+    char line[120];
+    struct lista *pNext;
+} LISTA;
 
+int main(int argc, char *argv[]) // komentoriviargumentit jotka otetaan kiinni
+{
+    size_t linesize = 0;
+    char *linebuffer;
+    size_t buffersize = 0; // alustetaan getlinea varten muuttujia
+    int linecount = 0;
+    char *tmp;
 
-    int main(int argc, char *argv[])   // komentoriviargumentit jotka otetaan kiinni  
-    {  
-        size_t linesize = 0;
-        char* linebuffer;
-        size_t buffersize=0; // alustetaan getlinea varten muuttujia
-        char* ptr;
-        char filecontent[300]; //Alustan vielä nyt stringin koon.
+    LISTA *pAlku = NULL, *pLoppu = NULL;
+    LISTA *pUusi, *ptr;
 
-        FILE* fp =fopen(argv[1], "r");
-        if(fp == NULL){
-            return 1;
-        }
-        linesize = getline(&linebuffer,&buffersize, fp); //eka rivi, jotta while true ehto toteutuu
-
-        //ptr = (char*) malloc(linesize *sizeof(char));
-        while(linesize >= 0){
-        
-        fscanf(fp, "%s\n", linebuffer);
-    
-        fprintf(stdout,linebuffer); 
-    
-        fflush(stdout);
-
-       
-
-        //linesize = getline(&linebuffer,&buffersize, fp); // uusi rivi
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL)
+    {
         return 1;
-        }
+    }
 
-        fclose(fp);
-    
+    while (linesize != -1)
+    {
+        linesize = getline(&linebuffer, &buffersize, fp);
        
-    }  
+        if (linesize == -1) {
+            break;
+        }
+         fprintf(stdout, linebuffer);
+        if ((pUusi = (LISTA *)malloc(sizeof(LISTA))) == NULL)
+        {
+            perror("malloc failed.");
+            exit(1);
+        }
+        
+        strcpy(pUusi->line, linebuffer);
+        pUusi->pNext = NULL;
+
+        if (pAlku == NULL)
+        {
+            pAlku = pUusi;
+            pLoppu = pUusi;
+            
+        }
+        else
+        {
+            pLoppu->pNext = pUusi;
+            pLoppu = pUusi;
+            
+        }
+       
+        
+    }
+    fclose(fp);
+    
+
+    
+
+}
